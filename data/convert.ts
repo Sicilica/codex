de425c6b0ab9bf7863b8aba8afd4719b4a30d7a2
Sicilica <Sicilica@users.noreply.github.com>
2020-07-05 21:07:13 -0600

    finish parsing data

--- data/convert.ts
+++ data/convert.ts
@@ -34,7 +34,7 @@ const convertCard = (
   } else if (firstInfoPart.includes("Upgrade")) {
     typeSpecificData = parseUpgradeInfo(infoLineParts, typeLineParts);
   } else if (firstInfoPart.includes("Hero")) {
-    typeSpecificData = parseHeroInfo(infoLineParts, typeLineParts);
+    typeSpecificData = parseHeroInfo(typeLineParts);
   } else {
     throw new Error(`Unable to parse card: ${raw.infoLine}`);
   }
@@ -64,21 +64,29 @@ const parseBuildingInfo = (
   infoParts: Array<string>,
   typeParts: Array<string>,
 ) => {
+  const { fullType, tags } = parseTagsAndFullType(infoParts[0]);
+  const legendary = fullType.includes("Legendary");
+  
+  const health = parseInt(parse(/^HP: (\d+)$/, infoParts[2]));
+
   return {
     type: "building",
-    infoParts,
-    typeParts,
+    ...parseTechCardTypeLine(typeParts),
+    health,
+    tags,
+    legendary,
   };
 };
 
 const parseHeroInfo = (
-  infoParts: Array<string>,
   typeParts: Array<string>,
 ) => {
+  const color = typeParts[0].toLowerCase();
+  const spec = parse(/^(\w+) Hero$/, typeParts[1]).toLowerCase();
   return {
     type: "hero",
-    infoParts,
-    typeParts,
+    color,
+    spec,
   };
 };
 
@@ -86,10 +94,27 @@ const parseSpellInfo = (
   infoParts: Array<string>,
   typeParts: Array<string>,
 ) => {
+  let color: string;
+  let spec: string | null;
+  if (typeParts[0].includes("Magic")) {
+    color = parse(/^(\w+) Magic$/, typeParts[0]).toLowerCase();
+    spec = null;
+  } else {
+    color = typeParts[0].toLowerCase();
+    spec = parse(/^(\w+) (Ultimate )?Magic$/, typeParts[1]).toLowerCase();
+  }
+
+  const { fullType, tags } = parseTagsAndFullType(infoParts[0]);
+  const ultimate = fullType.includes("Ultimate");
+  const ongoing = fullType.includes("Ongoing");
+
   return {
     type: "spell",
-    infoParts,
-    typeParts,
+    color,
+    spec,
+    tags,
+    ultimate,
+    ongoing,
   };
 };
 
@@ -97,10 +122,19 @@ const parseUnitInfo = (
   infoParts: Array<string>,
   typeParts: Array<string>,
 ) => {
+  const { fullType, tags } = parseTagsAndFullType(infoParts[0]);
+  const legendary = fullType.includes("Legendary");
+
+  const attack = parseInt(parse(/^ATK: (\d+)$/, infoParts[2]));
+  const health = parseInt(parse(/^HP: (\d+)$/, infoParts[3]));
+
   return {
     type: "unit",
-    infoParts,
-    typeParts,
+    ...parseTechCardTypeLine(typeParts),
+    attack,
+    health,
+    tags,
+    legendary,
   };
 };
 
@@ -108,11 +142,56 @@ const parseUpgradeInfo = (
   infoParts: Array<string>,
   typeParts: Array<string>,
 ) => {
+  const { fullType, tags } = parseTagsAndFullType(infoParts[0]);
+  const legendary = fullType.includes("Legendary");
+
   return {
     type: "upgrade",
-    infoParts,
-    typeParts,
+    ...parseTechCardTypeLine(typeParts),
+    legendary,
+    // Note that upgrades never actually have tags
+    tags,
   };
 };
+
+const parseTagsAndFullType = (
+  line: string,
+) => {
+  let fullType: string;
+  let tags: Array<string>;
+  if (line.includes("—")) {
+    fullType = parse(/^([\w ]+) —/, line);
+    tags = parse(/— ([\w ]+)$/, line).toLowerCase().split(" ");
+  } else {
+    fullType = line;
+    tags = [];
+  }
+  return {
+    fullType,
+    tags,
+  };
+};
+
+const parseTechCardTypeLine = (
+  typeParts: Array<string>,
+) => {
+  let color: string;
+  let spec: string | null;
+  let tech: number;
+  if (typeParts[0].includes("Tech 0")) {
+    color = parse(/^(\w+) Tech 0$/, typeParts[0]).toLowerCase();
+    spec = null;
+    tech = 0;
+  } else {
+    color = typeParts[0].toLowerCase();
+    spec = parse(/^(\w+) Tech I+$/, typeParts[1]).toLowerCase();
+    tech = parse(/^\w+ Tech (I+)$/, typeParts[1]).length;
+  }
+  return {
+    color,
+    spec,
+    tech,
+  };
+}
 
 main();
