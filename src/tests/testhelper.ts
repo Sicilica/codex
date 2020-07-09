880b63f6c87a91772ffea854616aeec87692953f
Zeimyth <zeimyth@hotmail.com>
2020-07-09 14:33:29 -0600

    Implement helper function for generating players for tests
    Should eventually be replaced with a function that generates players for the real game, which tests can then call directly.

--- src/tests/testhelper.ts
+++ src/tests/testhelper.ts
@@ -1,4 +1,4 @@
-import { GameState } from "../framework/types";
+import { GameState, PlayerState, Spec } from "../framework/types";
 
 export const P1 = "P1";
 export const P2 = "P2";
@@ -9,80 +9,48 @@ export const initDummyGameState: () => GameState = () => {
     instances: {},
     nextID: 100,
     players: {
-      [P1]: {
-        addon: null,
-        base: {
-          damage: 0,
-        },
-        gold: 0,
-        specs: [ "ANARCHY", "BLOOD", "FIRE" ],
-        mainSpec: null,
-        hand: [],
-        discard: [],
-        deck: [],
-        hasShuffledThisTurn: false,
-        patrol: {
-          squadLeader: null,
-          elite: null,
-          scavenger: null,
-          technician: null,
-          lookout: null,
-        },
-        techBuildings: [
-          {
-            damage: 0,
-            purchased: false,
-            ready: false,
-          },
-          {
-            damage: 0,
-            purchased: false,
-            ready: false,
-          },
-          {
-            damage: 0,
-            purchased: false,
-            ready: false,
-          },
-        ],
-      },
-      [P2]: {
-        addon: null,
-        base: {
-          damage: 0,
-        },
-        gold: 0,
-        specs: [ "BALANCE", "FERAL", "GROWTH" ],
-        mainSpec: null,
-        hand: [],
-        discard: [],
-        deck: [],
-        hasShuffledThisTurn: false,
-        patrol: {
-          squadLeader: null,
-          elite: null,
-          scavenger: null,
-          technician: null,
-          lookout: null,
-        },
-        techBuildings: [
-          {
-            damage: 0,
-            purchased: false,
-            ready: false,
-          },
-          {
-            damage: 0,
-            purchased: false,
-            ready: false,
-          },
-          {
-            damage: 0,
-            purchased: false,
-            ready: false,
-          },
-        ],
-      },
+      [P1]: generatePlayer([ "ANARCHY", "BLOOD", "FIRE" ]),
+      [P2]: generatePlayer([ "BALANCE", "FERAL", "GROWTH" ]),
     },
+  };
+};
+
+const generatePlayer = (specs: [Spec, Spec, Spec]): PlayerState => {
+  return {
+    addon: null,
+    base: {
+      damage: 0,
+    },
+    gold: 0,
+    specs,
+    mainSpec: null,
+    hand: [],
+    discard: [],
+    deck: [],
+    hasShuffledThisTurn: false,
+    patrol: {
+      squadLeader: null,
+      elite: null,
+      scavenger: null,
+      technician: null,
+      lookout: null,
+    },
+    techBuildings: [
+      {
+        damage: 0,
+        purchased: false,
+        ready: false,
+      },
+      {
+        damage: 0,
+        purchased: false,
+        ready: false,
+      },
+      {
+        damage: 0,
+        purchased: false,
+        ready: false,
+      },
+    ],
   };
 };
