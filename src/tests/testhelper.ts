120ebb941294badfd3f68ab28a120551e7afffca
Zeimyth <zeimyth@hotmail.com>
2020-07-09 16:21:29 -0600

    Implement global turn phases
    Under this system, TECH is a pre-turn phase. Any teching during opponent's turns should be handled client-side

--- src/tests/testhelper.ts
+++ src/tests/testhelper.ts
@@ -5,29 +5,38 @@ export const P2 = "P2";
 
 export const initDummyGameState: () => GameState = () => {
   return {
+    firstPlayer: P1,
+    round: 1,
     activePlayer: P1,
+    turnPhase: "MAIN",
     instances: {},
     nextID: 100,
     players: {
-      [P1]: generatePlayer([ "ANARCHY", "BLOOD", "FIRE" ]),
+      [P1]: generatePlayer([ "ANARCHY", "BLOOD", "FIRE" ], true),
       [P2]: generatePlayer([ "BALANCE", "FERAL", "GROWTH" ]),
     },
   };
 };
 
-const generatePlayer = (specs: [Spec, Spec, Spec]): PlayerState => {
+const generatePlayer = (
+  specs: [Spec, Spec, Spec],
+  first = false
+): PlayerState => {
   return {
     addon: null,
     base: {
       damage: 0,
     },
+    workers: first ? 4 : 5,
     gold: 0,
     specs,
     mainSpec: null,
     hand: [],
     discard: [],
     deck: [],
+    canSkipTech: false,
     hasShuffledThisTurn: false,
+    hasBuiltWorkerThisTurn: false,
     patrol: {
       squadLeader: null,
       elite: null,
