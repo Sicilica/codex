ec020cc845adb7d56ed5fec50ecc5849cdc85a3d
Sicilica <Sicilica@users.noreply.github.com>
2020-11-01 16:48:58 -0700

    impl tests for rulings

--- src/tests/spells/red.ts
+++ src/tests/spells/red.ts
@@ -1,10 +1,93 @@
 import { expect } from "chai";
 
+import { BASE_CARD } from "../../data/core";
+import { GameEngine } from "../../framework/engine";
+import { createInstance } from "../../framework/mutators";
+import { PlayerID, PlayerState } from "../../framework/types";
+import { requireActivePlayer } from "../../game/helpers";
+
+import {
+  P2,
+  debugAction,
+  debugPlayCard,
+  makeDefaultGame,
+} from "../testhelper";
+
 describe("spells", () => {
   describe("red", () => {
+    let $: GameEngine;
+    let P: PlayerState;
+    let oppP: PlayerState;
+
+    beforeEach(() => {
+      $ = makeDefaultGame();
+      P = requireActivePlayer($);
+      oppP = $.state.players[P2];
+    });
+
     describe("Pillage", () => {
+      beforeEach(() => {
+        const EX_RED_HERO = $.data.lookupCard("Captain Zane");
+        createInstance($, P, EX_RED_HERO);
+      });
+
+      const playAndResolvePillage = (
+        targetPlayer: PlayerID,
+      ): void => {
+        debugPlayCard($, "Pillage");
+        expect($.state.unresolvedEffects.length).to.equal(1);
+        const eid = $.state.unresolvedEffects[0].id;
+
+        const base = $.findInstance({
+          card: BASE_CARD.id,
+          player: targetPlayer,
+        });
+        if (base == null) {
+          throw new Error("failed to find base for pillage");
+        }
+
+        debugAction($, {
+          type: "RESOLVE_EFFECT",
+          effect: eid,
+          params: {
+            target: base.id,
+          },
+        });
+      };
+
       it("can't steal more gold than the target has", () => {
-        expect(true).to.equal(false);
+        P.gold = 0;
+        oppP.gold = 0;
+        playAndResolvePillage(oppP.id);
+        expect(P.gold).to.equal(0);
+        expect(oppP.gold).to.equal(0);
+
+        P.gold = 0;
+        oppP.gold = 1;
+        playAndResolvePillage(oppP.id);
+        expect(P.gold).to.equal(1);
+        expect(oppP.gold).to.equal(0);
+
+        const EX_PIRATE_CARD = $.data.lookupCard("Bombaster");
+        createInstance($, P, EX_PIRATE_CARD);
+
+        P.gold = 0;
+        oppP.gold = 1;
+        playAndResolvePillage(oppP.id);
+        expect(P.gold).to.equal(1);
+        expect(oppP.gold).to.equal(0);
+
+        P.gold = 0;
+        oppP.gold = 2;
+        playAndResolvePillage(oppP.id);
+        expect(P.gold).to.equal(2);
+        expect(oppP.gold).to.equal(0);
+
+        P.gold = 0;
+        oppP.gold = 3;
+        playAndResolvePillage(oppP.id);
+        expect(P.gold).to.equal(2);
+        expect(oppP.gold).to.equal(1);
       });
     });
   });
--- src/tests/upgrades/red.ts
+++ src/tests/upgrades/red.ts
@@ -1,11 +1,56 @@
 import { expect } from "chai";
 
+import { GameEngine } from "../../framework/engine";
+import { createInstance } from "../../framework/mutators";
+import { InstanceState, PlayerState } from "../../framework/types";
+import { requireActivePlayer } from "../../game/helpers";
+
+import {
+  debugAction,
+  debugGotoNextTurn,
+  makeDefaultGame,
+} from "../testhelper";
+
 describe("upgrades", () => {
   describe("red", () => {
+    let $: GameEngine;
+    let P: PlayerState;
+
+    beforeEach(() => {
+      $ = makeDefaultGame();
+      P = requireActivePlayer($);
+    });
+
     describe("Bloodburn", () => {
+      let I: InstanceState;
+
+      beforeEach(() => {
+        I = createInstance($, P, $.data.lookupCard("Bloodburn"));
+      });
+
       it("can't be used on its first turn because it doesn't have " +
         "haste", () => {
-        expect(true).to.equal(false);
+        expect(I.arrivalFatigue).to.equal(true);
+
+        I.customRunes.BLOOD = 2;
+
+        expect(
+          () => debugAction($, {
+            type: "ACTIVATE_ABILITY",
+            instance: I.id,
+            ability: "Bloodburn #1",
+          }),
+        ).to.throw("instance cannot attack on the turn it arrived");
+
+        debugGotoNextTurn($, P.id);
+
+        expect(
+          () => debugAction($, {
+            type: "ACTIVATE_ABILITY",
+            instance: I.id,
+            ability: "Bloodburn #1",
+          }),
+        ).not.to.throw();
       });
     });
   });
