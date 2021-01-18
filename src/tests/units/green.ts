9760884731f09a612b74e08ccf1b14eef4423c3f
Zeimyth <zeimyth@hotmail.com>
2021-01-18 12:09:31 -0700

    Implement Spore Shambler

--- src/tests/units/green.ts
+++ src/tests/units/green.ts
@@ -7,6 +7,8 @@ import { requireActivePlayer } from "../../game/helpers";
 
 import {
   debugAction,
+  debugAutoResolve,
+  debugValidateEffects,
   makeDefaultGame,
 } from "../testhelper";
 
@@ -42,6 +44,133 @@ describe("units", () => {
 
         expect(P.gold).to.equal(before + 1);
         expect(I.readyState).to.equal("EXHAUSTED");
+      });
+    });
+
+    describe("Spore Shambler", () => {
+      let I: InstanceState;
+
+      beforeEach(() => {
+        I = createInstance($, P, $.data.lookupCard("Spore Shambler"));
+        I.arrivalFatigue = false;
+        debugAutoResolve($);
+      });
+
+      it("arrives with 2 +1/+1 runes", () => {
+        expect(I.plusMinusRunes).to.equal(2);
+      });
+
+      it("can give a +1/+1 rune to an ally unit by exhausting", () => {
+        const I2 = createInstance($, P, $.data.lookupCard("Tiger Cub"));
+
+        debugAction(
+          $,
+          {
+            type: "ACTIVATE_ABILITY",
+            instance: I.id,
+            ability: `${I.card} #1`,
+          }
+        );
+
+        debugValidateEffects($);
+        expect($.state.unresolvedEffects.length).to.equal(1);
+        const effectID = $.state.unresolvedEffects[0].id;
+
+        debugAction($, {
+          type: "RESOLVE_EFFECT",
+          effect: effectID,
+          params: {
+            target: I2.id,
+          },
+        });
+
+        expect(I.plusMinusRunes).to.equal(1);
+        expect(I.readyState).to.equal("EXHAUSTED");
+        expect(I2.plusMinusRunes).to.equal(1);
+      });
+
+      it("can give a +1/+1 rune to an ally unit by paying gold", () => {
+        const I2 = createInstance($, P, $.data.lookupCard("Tiger Cub"));
+
+        debugAction(
+          $,
+          {
+            type: "ACTIVATE_ABILITY",
+            instance: I.id,
+            ability: `${I.card} #2`,
+          }
+        );
+
+        debugValidateEffects($);
+        expect($.state.unresolvedEffects.length).to.equal(1);
+        const effectID = $.state.unresolvedEffects[0].id;
+
+        debugAction($, {
+          type: "RESOLVE_EFFECT",
+          effect: effectID,
+          params: {
+            target: I2.id,
+          },
+        });
+
+        expect(P.gold).to.equal(3);
+        expect(I.plusMinusRunes).to.equal(1);
+        expect(I.readyState).to.equal("READY");
+        expect(I2.plusMinusRunes).to.equal(1);
+
+        // Can trigger more than once:
+
+        debugAction(
+          $,
+          {
+            type: "ACTIVATE_ABILITY",
+            instance: I.id,
+            ability: `${I.card} #2`,
+          }
+        );
+
+        debugValidateEffects($);
+        expect($.state.unresolvedEffects.length).to.equal(1);
+        const effectID2 = $.state.unresolvedEffects[0].id;
+
+        debugAction($, {
+          type: "RESOLVE_EFFECT",
+          effect: effectID2,
+          params: {
+            target: I2.id,
+          },
+        });
+
+        expect(P.gold).to.equal(2);
+        expect(I.plusMinusRunes).to.equal(0);
+        expect(I.readyState).to.equal("READY");
+        expect(I2.plusMinusRunes).to.equal(2);
+      });
+
+      it("cannot give +1/+1 runes to heroes", () => {
+        const I2 = createInstance($, P, $.data.lookupCard("Master Midori"));
+        createInstance($, P, $.data.lookupCard("Tiger Cub"));
+
+        debugAction(
+          $,
+          {
+            type: "ACTIVATE_ABILITY",
+            instance: I.id,
+            ability: `${I.card} #1`,
+          }
+        );
+
+        debugValidateEffects($);
+        expect($.state.unresolvedEffects.length).to.equal(1);
+        const effectID = $.state.unresolvedEffects[0].id;
+
+        expect(() => debugAction($, {
+          type: "RESOLVE_EFFECT",
+          effect: effectID,
+          params: {
+            target: I2.id,
+          },
+        })).to.throw("invalid params for effect: invalid param [target]");
       });
     });
   });
