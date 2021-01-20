362b504c1ade41fa38111be797ceb56be71ad922
Zeimyth <zeimyth@hotmail.com>
2021-01-19 21:36:54 -0700

    Implement as much of Rich Earth and Verdant Tree as possible

--- src/data/buildings.ts
+++ src/data/buildings.ts
@@ -3,6 +3,23 @@ import { getProperties } from "./helpers";
 
 export const getBuildingProperties = getProperties(id => {
   switch (id) {
+  case "Verdant Tree":
+    // WIP
+    // No way to target bases
+    // No way to say "Your tech buildings build instantly"
+    return {
+      attributes: {
+        HEALING: 1,
+      },
+      // activatedAbilities: [
+      //   active(id, aid++, [
+      //     {
+      //       type: "EXHAUST_THIS",
+      //     },
+      //   ],
+      //   )
+      // ]
+    };
   default:
     if (REQUIRE_ALL_CARD_PROPERTIES) {
       throw new Error(`Failed to find properties for building "${id}"`);
