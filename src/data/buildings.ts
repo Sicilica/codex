149a303618bba278ce422165cf98062d6fb1bbc4
Zeimyth <zeimyth@hotmail.com>
2021-01-24 22:11:25 -0700

    Implement Verdant Tree

--- src/data/buildings.ts
+++ src/data/buildings.ts
@@ -1,24 +1,61 @@
 import { REQUIRE_ALL_CARD_PROPERTIES } from "./config";
-import { getProperties } from "./helpers";
+import {
+  active,
+  constantModifiers,
+  effectBase,
+  getProperties,
+  queryParam,
+} from "./helpers";
 
 export const getBuildingProperties = getProperties(id => {
+  let aid = 1;
   switch (id) {
   case "Verdant Tree":
-    // WIP
-    // No way to target bases
-    // No way to say "Your tech buildings build instantly"
     return {
       attributes: {
         HEALING: 1,
       },
-      // activatedAbilities: [
-      //   active(id, aid++, [
-      //     {
-      //       type: "EXHAUST_THIS",
-      //     },
-      //   ],
-      //   )
-      // ]
+      activatedAbilities: [
+        active(
+          id,
+          aid++,
+          [
+            {
+              type: "EXHAUST_THIS",
+            },
+          ],
+          ($, I) => {
+            const player = $.getPlayer(I.controller);
+
+            if (player != null) {
+              // See extensive note in Verdant Tree unit tests
+              return [
+                {
+                  ...effectBase(id, I, "MODIFY"),
+                  target: queryParam("INSTANCE", {
+                    card: [
+                      "Tech I Building",
+                      "Tech II Building",
+                      "Tech III Building",
+                    ],
+                  }),
+                  modifiers: constantModifiers([
+                    {
+                      effect: {
+                        type: "TRAIT",
+                        trait: "HASTE",
+                      },
+                      expiration: "END_OF_TURN",
+                      sourceCard: id,
+                    },
+                  ]),
+                },
+              ];
+            }
+            return [];
+          },
+        ),
+      ],
     };
   default:
     if (REQUIRE_ALL_CARD_PROPERTIES) {
