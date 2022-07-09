c1283512bfe9fb0f58f965275871321fd56785cd
Zeimyth <zeimyth@hotmail.com>
2022-07-09 01:39:35 -0600

    Implement unit tests for Bombaster

--- src/tests/units/red.ts
+++ src/tests/units/red.ts
@@ -99,6 +99,111 @@ describe("units", () => {
       );
     });
 
+    describe("Bombaster", () => {
+      let I: InstanceState;
+
+      beforeEach(() => {
+        I = createInstance($, P, $.data.lookupCard("Bombaster"));
+      });
+
+      it("is a pirate", () => {
+        // eslint-disable-next-line no-unused-expressions
+        expect(
+          $.findInstance({
+            player: P.id,
+            tags: [ "PIRATE" ],
+          })
+        ).to.not.be.null;
+      });
+
+      describe("active ability", () => {
+        it("requires enough gold", () => {
+          P.gold = 0;
+
+          expect(
+            () => debugAction($, {
+              type: "ACTIVATE_ABILITY",
+              instance: I.id,
+              ability: `${I.card} #1`,
+            })
+          ).to.throw("not enough gold");
+        });
+
+        it("works as expected", () => {
+          const I2 = createInstance($, oppP, $.data.lookupCard("Tiger Cub"));
+
+          const startingGold = P.gold;
+
+          oppP.patrol.ELITE = I2.id;
+
+          debugAction($, {
+            type: "ACTIVATE_ABILITY",
+            instance: I.id,
+            ability: `${I.card} #1`,
+          });
+
+          debugAutoResolve($);
+
+          expect(P.gold).to.equal(startingGold - 1);
+          expect(I2.damage).to.equal(2);
+
+          // eslint-disable-next-line no-unused-expressions
+          expect($.getInstance(I.id)).to.be.undefined;
+        });
+
+        it("can only target patrolling units", () => {
+          const oppI = createInstance($, oppP, $.data.lookupCard("Tiger Cub"));
+          const oppI2 =
+            createInstance($, oppP, $.data.lookupCard("Tiger Cub"));
+          const oppI3 =
+            createInstance($, oppP, $.data.lookupCard("Tiger Cub"));
+          oppP.patrol.ELITE = oppI2.id;
+          oppP.patrol.SCAVENGER = oppI3.id;
+
+          const startingGold = P.gold;
+
+          debugAction($, {
+            type: "ACTIVATE_ABILITY",
+            instance: I.id,
+            ability: `${I.card} #1`,
+          });
+
+          expect($.state.unresolvedEffects.length).to.equal(2);
+          const eid = $.state.unresolvedEffects[1].id;
+
+          expect(
+            () => debugAction($, {
+              type: "RESOLVE_EFFECT",
+              effect: eid,
+              params: {
+                target: oppI.id,
+              },
+            })
+          ).to.throw("invalid params for effect");
+
+          debugAction($, {
+            type: "RESOLVE_EFFECT",
+            effect: eid,
+            params: {
+              target: oppI2.id,
+            },
+          });
+
+          debugAutoResolve($);
+
+          expect(P.gold).to.equal(startingGold - 1);
+          expect(oppI.damage).to.equal(0);
+          expect(oppI2.damage).to.equal(2);
+          expect(oppI3.damage).to.equal(0);
+
+          // eslint-disable-next-line no-unused-expressions
+          expect($.getInstance(I.id)).to.be.undefined;
+        });
+
+        it.skip("triggers targeting effects");
+      });
+    });
+
     describe("Nautical Dog", () => {
       let I: InstanceState;
 
